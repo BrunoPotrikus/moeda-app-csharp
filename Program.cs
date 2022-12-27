28b554100f2f89d051b57902f9fcd3c04a068204
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-26 21:46:00 -0300

    Primeiro commit, com padronização de moedas

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var valor = 10.25;
+
+        var culture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+        // "C" coloca o valor com padrão de moeda, ex: R$, $. 
+        Console.WriteLine(valor.ToString("C", culture));   
+    }
+}
+
